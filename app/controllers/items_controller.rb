ecfad5ab0cf389cfd6d4a1a793f087479639a73e
valuset3 <302peni@gmail.com>
2022-10-16 07:28:05 +0900

    メソッドを１つにまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  # before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless @item.user == current_user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      render action: :show
+      redirect_to item_path(item_params)
     else
       render action: :edit
     end
@@ -44,9 +41,7 @@ class ItemsController < ApplicationController
                                  :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
-  # def move_to_index
-  #   unless user_signed_in? == current_user.
-  #     redirect_to root_path
-  #   end
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
