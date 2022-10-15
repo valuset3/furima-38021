2560c2aa7208ce6a033b0dcec22386b81e9c721d
valuset3 <302peni@gmail.com>
2022-10-15 19:37:43 +0900

    showアクションに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      render action: :show
     else
       render action: :edit
     end
