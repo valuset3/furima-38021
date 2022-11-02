572b23a77d25d82f114bee05eda1788dd37940d5
valuset3 <302peni@gmail.com>
2022-11-03 07:20:54 +0900

    県のバリデーション

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -9,7 +9,7 @@ class OrderForm
     validates :item_id
     # paymentモデルのバリデーション
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid' }
