109373a1fda71cbaa9f4349fab30fe0d739ae08a
valuset3 <302peni@gmail.com>
2023-01-14 22:25:19 +0900

    previewの導入

---
+++ app/javascript/preview.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+  // 新規投稿・編集ページのフォームを取得
+  const postForm = document.getElementById('new');
+  // プレビューを表示するためのスペースを取得
+  const previewList = document.getElementById('previews');
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+  // if (!postForm) return null;
+  console.log("preview.jsが読み込まれましたよん");
+});
